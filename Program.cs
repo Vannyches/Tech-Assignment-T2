b93141f046de2fa4bcc4c67ec351a6d53301b8bf
Vanny Ches <vches@usf.edu>
2022-01-20 20:02:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Program Developed by: Vanny Ches
+using System;
+
+namespace Tech_Assignment_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sometimes you don't get what you want because you deserve better.");
+        }
+    }
+}
